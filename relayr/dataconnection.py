cd45d01981805bc08a6546439c996af58b6b0532
Dinu Gherman <gherman@darwin.in-berlin.de>
2015-05-08 17:34:22 +0200

    Removed pubnub, replaced using dedicated certificate with generic certifi one.

--- relayr/dataconnection.py
+++ relayr/dataconnection.py
@@ -10,111 +10,17 @@ import sys
 import ssl
 import time
 import threading
-import platform
-import warnings
-import os
-from os.path import exists, join, expanduser, basename
+# import platform
+# import warnings
+# import os
+# from os.path import exists, join, expanduser, basename
 
 import requests
+import certifi
 import paho.mqtt.client as mqtt
 
 from relayr import config
 from relayr.compat import PY2, PY3
-
-# This will be replaced lazily with the Pubnub class from the Pubnub
-# package, iff needed (before soon removing PubNub supprot completely).
-Pubnub = None
-
-
-class PubnubDataConnection(threading.Thread):
-    "A connection to a PubNub data hub running on its own thread."
-
-    def __init__(self, callback, credentials):
-        """
-        Opens a PubNub connection with a callback and a credentials dict.
-
-        :param callback: A callable to be called with two arguments:
-            message_content and channel_name.
-        :type callback: A function or object implementing the ``__call__`` method.
-        :param credentials: A set of key/value pairs to be passed to PubNub.
-        :type credentials: dict
-        """
-
-        super(PubnubDataConnection, self).__init__()
-        self._stop_event = threading.Event()
-        self.callback = callback
-        self.credentials = credentials
-        self.channel = credentials['channel']
-
-        # lazy import of Pubnub (might need a 'sudo' depending on Python installation)
-        global Pubnub
-        if not Pubnub:
-            msg = "Pubnub support will be discontinued in relayr==0.3.0, please use MQTT!"
-            warnings.warn(msg, RuntimeWarning)
-            try:
-                from Pubnub import Pubnub
-            except ImportError:
-                import pip
-                pip.main(['install', 'pubnub==3.7.1'])
-                try:
-                    from Pubnub import Pubnub
-                except ImportError:
-                    msg = "Failed to install pubnub==3.7.1 at runtime. "
-                    msg += "Maybe re-run with 'sudo' or execute "
-                    msg += "'pip install pubnub==3.7.1' separately..."
-                    raise RuntimeError(msg)
-
-        self.hub = Pubnub(
-            publish_key=credentials.get('publishKey', None),
-            subscribe_key=credentials.get('subscribeKey', None),
-            cipher_key=credentials.get('cipherKey', None),
-            auth_key=credentials.get('authKey', None),
-            secret_key=None,
-            ssl_on=True
-        )
-
-        if PY2 and "daemon" in Pubnub.__init__.func_code.co_varnames:
-            self.hub.daemon = True
-        elif PY3 and "daemon" in Pubnub.__init__.__code__.co_varnames:
-            self.hub.daemon = True
-
-        self.setDaemon(True)
-
-    def run(self):
-        """Thread method, called implicitly after starting the thread."""
-
-        self.subscribe(self.channel, self.callback)
-        while not self._stop_event.is_set():
-            time.sleep(1)
-
-    def stop(self):
-        """Mark the connection/thread for being stopped."""
-
-        self._stop_event.set()
-        self.unsubscribe(self.channel)
-
-    def subscribe(self, channel_name, callback):
-        """
-        Subscribes a callable to a channel with the given name.
-
-        :param channel_name: The channel name.
-        :type channel_name: string
-        :param callback: The callable to be called with two arguments:
-            message_content and channel_name.
-        :type callback: A function or object implementing the ``__call__`` method.
-        """
-
-        self.hub.subscribe(channel_name, callback)
-
-    def unsubscribe(self, channel_name):
-        """
-        Unsubscribes from a channel with given name.
-
-        :param channel_name: the channel name
-        :type channel_name: string
-        """
-
-        self.hub.unsubscribe(channel_name)
 
 
 class MqttStream(threading.Thread):
@@ -141,6 +47,7 @@ class MqttStream(threading.Thread):
             for credentials in self.credentials_list]
         self.setDaemon(True)
 
+    ## TODO: remove
     def _fetch_certificate(self):
         """
         Fetch certificate for accessing MQTT server and cache it.
@@ -169,21 +76,25 @@ class MqttStream(threading.Thread):
         c.on_unsubscribe = self.on_unsubscribe
         c.username_pw_set(creds['user'], creds['password'])
 
-        # only encryption, no authentication
-        c.tls_insecure_set(True)
-        folder = expanduser(config.RELAYR_FOLDER)
-        cert_url = config.MQTT_CERT_URL
-        cert_filename = basename(cert_url)
-        if not exists(join(folder, cert_filename)):
-            self._fetch_certificate()
-        cert_path = join(folder, cert_filename)
-        c.tls_set(ca_certs=cert_path)
+        if 0:
+            # only encryption, no authentication
+            # c.tls_insecure_set(True)
+            folder = expanduser(config.RELAYR_FOLDER)
+            cert_url = config.MQTT_CERT_URL
+            cert_filename = basename(cert_url)
+            if not exists(join(folder, cert_filename)):
+                self._fetch_certificate()
+            cert_path = join(folder, cert_filename)
+            # c.tls_set(ca_certs=cert_path)
+        c.tls_set(certifi.where(), tls_version=ssl.PROTOCOL_TLSv1)
+        c.connect('mqtt.relayr.io', port=8883, keepalive=60)
 
-        try:
-            c.connect('mqtt.relayr.io', port=8883, keepalive=60)
-        except: # invalid cert?
-            self._fetch_certificate()
-            c.connect('mqtt.relayr.io', port=8883, keepalive=60)
+        if 0:
+            try:
+                c.connect('mqtt.relayr.io', port=8883, keepalive=60)
+            except: # invalid cert?
+                self._fetch_certificate()
+                c.connect('mqtt.relayr.io', port=8883, keepalive=60)
 
         try:
             c.loop_forever()
@@ -257,9 +168,3 @@ class MqttStream(threading.Thread):
         if PY2:
            topic = topic.encode('utf-8')
         self.client.unsubscribe(topic)
-
-
-if config.dataConnectionHubName == 'PubNub':
-    Connection = PubnubDataConnection
-elif config.dataConnectionHubName == 'MQTT':
-    Connection = MqttStream
